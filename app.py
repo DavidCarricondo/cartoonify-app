fbca48d26c0991874116ad5dcf65b915f9043aa5
david.carricondo <david.carricondo@clarity.ai>
2021-04-22 07:53:53 +0200

    Created template html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash, url_for
 import requests
 from src.utils import CARTOON
 
@@ -6,12 +6,14 @@ app = Flask('__name__')
 
 @app.route('/')
 def welcome():
-    return 'Welcome to my cartoonify (WIP)'
+    return render_template('index.html')
+
+
 ##TODO:
-@app.route('/cartoon', methods=['POST'])
+@app.route('/<image_path>', methods=['POST'])
 def cartoon():
-    picture = requests.data
-    return CARTOON().fit_transform('models/Hosoda_net_G_float.pth', picture)
+    output_image = CARTOON().fit_transform('models/Hosoda_net_G_float.pth', image_path)
+    return 'Did it, good job'
 
 if __name__ == '__main__':
     app.run('0.0.0.0',5000, debug=True)
